94819b0663e8703d1351e2704559825513a4ad1d
Dustin Dally <dj.dally@protonmail.com>
2022-08-15 17:43:39 -0600

    Got Emoji to work
    Need to make it open to the original page.

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 // Create random number generator
-
-
-var randomNumber1 = Math.floor(Math.random() * 5);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 const dice1 = document.querySelector(".img1");
-var randomNumber2 = Math.floor(Math.random() * 5);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 const dice2 = document.querySelector(".img2");
 
 // Determine Dice 1
-if (randomNumber1 === 0) {
+if (randomNumber1 === 1) {
   dice1.setAttribute("src","images/dice1.png");
-} else if (randomNumber1 === 1) {
-  dice1.setAttribute("src","images/dice2.png");
 } else if (randomNumber1 === 2) {
-  dice1.setAttribute("src","images/dice3.png");
+  dice1.setAttribute("src","images/dice2.png");
 } else if (randomNumber1 === 3) {
-  dice1.setAttribute("src","images/dice4.png");
+  dice1.setAttribute("src","images/dice3.png");
 } else if (randomNumber1 === 4) {
+  dice1.setAttribute("src","images/dice4.png");
+} else if (randomNumber1 === 5) {
   dice1.setAttribute("src","images/dice5.png");
 } else {
   dice1.setAttribute("src","images/dice6.png");
 }
 
 // Determine Dice 2
-if (randomNumber2 === 0) {
+if (randomNumber2 === 1) {
   dice2.setAttribute("src","images/dice1.png");
-} else if (randomNumber2 === 1) {
-  dice2.setAttribute("src","images/dice2.png");
 } else if (randomNumber2 === 2) {
-  dice2.setAttribute("src","images/dice3.png");
+  dice2.setAttribute("src","images/dice2.png");
 } else if (randomNumber2 === 3) {
-  dice2.setAttribute("src","images/dice4.png");
+  dice2.setAttribute("src","images/dice3.png");
 } else if (randomNumber2 === 4) {
+  dice2.setAttribute("src","images/dice4.png");
+} else if (randomNumber2 === 5) {
   dice2.setAttribute("src","images/dice5.png");
 } else {
   dice2.setAttribute("src","images/dice6.png");
@@ -38,23 +36,9 @@ if (randomNumber2 === 0) {
 
 // Show the winner
 if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Refresh Me!";
+  document.querySelector("h1").innerHTML = String.fromCodePoint(0x1F6A9) + "Draw!" + String.fromCodePoint(0x1F6A9);
 } else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Play 1 WINS!";
+  document.querySelector("h1").innerHTML = String.fromCodePoint(0x1F6A9) + "Player 1 wins!";
 } else {
-  document.querySelector("h1").innerHTML = "Play 2 WINS!";
+  document.querySelector("h1").innerHTML = "Player 2 wins!" + String.fromCodePoint(0x1F6A9);
 }
-
-// if (randomNumber === 0) {
-//   document.getElementsByClassName(."img1").setAttribute(src = "images/dice1.png");
-// } else if (randomNumber === 1) {
-//   document.getElementsByClassName(."img1").setAttribute(src = "images/dice2.png");
-// } else if (randomNumber === 2) {
-//   document.getElementsByClassName(."img1").setAttribute(src = "images/dice3.png");
-// } else if (randomNumber === 3) {
-//   document.getElementsByClassName(."img1").setAttribute(src = "images/dice4.png");
-// } else if (randomNumber === 4) {
-//   document.getElementsByClassName(."img1").setAttribute(src = "images/dice5.png");
-// } else {
-//   document.getElementsByClassName(."img1").setAttribute(src = "images/dice6.png");
-// }
